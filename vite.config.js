cbafa0ab70da59e8c3a885e13b24e5b8ea156b99
dpace121 <kayasthadipesh99@gmail.com>
2026-09-20 15:52:42 +0545

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss(),
-],
+  base: '/react-portfolio/', // 👈 Add this line (match your repo name exactly)
+  plugins: [react(), tailwindcss()],
 })
